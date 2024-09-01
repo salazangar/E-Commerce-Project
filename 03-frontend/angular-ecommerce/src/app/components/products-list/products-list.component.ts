ff729de978cf3b298b424f7edef884e04efd834c
salazangar <sidharthm673@gmail.com>
2024-09-01 18:30:15 +0530

    displaying in a table

--- 03-frontend/angular-ecommerce/src/app/components/products-list/products-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/products-list/products-list.component.ts
@@ -4,7 +4,8 @@ import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-products-list',
-  templateUrl: './products-list.component.html',
+  // templateUrl: './products-list.component.html',
+  templateUrl: './products-list-table.component.html',
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
