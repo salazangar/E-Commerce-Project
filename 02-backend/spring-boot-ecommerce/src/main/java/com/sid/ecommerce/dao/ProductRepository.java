5044fedf7a3803669bed9246eeb2a1262a270b97
salazangar <sidharthm673@gmail.com>
2024-09-01 18:10:33 +0530

    adding CORS support for fixing error

--- 02-backend/spring-boot-ecommerce/src/main/java/com/sid/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/sid/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,9 @@ package com.sid.ecommerce.dao;
 
 import com.sid.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
