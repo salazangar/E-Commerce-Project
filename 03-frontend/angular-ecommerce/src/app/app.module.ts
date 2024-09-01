56d314361304a163b360c7fd6fb4110c293ebeb0
salazangar <sidharthm673@gmail.com>
2024-09-01 13:30:27 +0530

    adding code to service to get response and unwrap data

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
