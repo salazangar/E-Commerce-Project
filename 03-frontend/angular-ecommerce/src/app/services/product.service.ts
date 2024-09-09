fa982f8c44d7b55e054300ad2a5886443024d115
salazangar <sidharthm673@gmail.com>
2024-09-09 20:11:12 +0530

    updating the service

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -8,17 +8,18 @@ import { Product } from '../common/product';
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products?';
+  private baseUrl = 'http://localhost:8080/api/products';
 
   constructor(private httpClient: HttpClient ) {}
 
   getProductList(theCategoryId: number ): Observable<Product[]> {
 
-    //@TODO: build URL based on category id  
+    // build URL based on category id  
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-      return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
-        map(response => response._embedded.products)
-      );
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
   }
 }
 
