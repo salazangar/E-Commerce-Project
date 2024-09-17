94104bdca3e3f468743e5ddc938a1945be67d150
salazangar <sidharthm673@gmail.com>
2024-09-17 13:32:30 +0530

    showing cart items in cart

---
+++ 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent implements OnInit {
+
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+    
+    // get handle on cart items
+    this.cartItems = this.cartService.cartItems;
+
+    // subscribe to total price and quantity
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+    
+    // compute totals
+    this.cartService.computeCartTotals();
+  }
+
+}
